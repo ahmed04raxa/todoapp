7a6dfd8ee45ad8dde44a431349343814545a7440
ahmed04raxa <ahmed04raxa@gmail.com>
2025-12-12 15:04:44 +0500

    first commit

---
+++ lib/repository/screen/splash/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:todo_app/repository/screen/home/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()),
+      );
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("SPLASH SCREEN")],
+      ),
+    );
+  }
+}
